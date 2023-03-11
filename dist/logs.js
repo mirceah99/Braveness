e48f89a968f5913cfd8a0da1e19c820d39691f86
Mircea Hanghiuc <mirceah99@gmail.com>
2023-03-11 21:09:38 +0200

    version 1.2.0

--- dist/logs.js
+++ dist/logs.js
@@ -12,16 +12,19 @@ class Bootstrap {
     static myLog(...args) {
         const error = this.generateError();
         const lineRegExp = /:[0-9]{1,9}:[0-9]{1,9}(\)?)/gm;
+        const pathRegExp = /(?<=\().*(:)/gm;
         const stackAsArray = error.stack.split("\n");
-        const errorStackLength = stackAsArray.length;
-        const highestLevelStack = stackAsArray[errorStackLength - 2];
+        const moduleStackIndex = stackAsArray.findIndex(stack => stack.includes('Module._compile'));
+        const highestLevelStack = stackAsArray[moduleStackIndex - 1];
+        let stackPath = highestLevelStack.match(pathRegExp)[0];
+        stackPath = stackPath.substring(0, stackPath.length - 1);
         const matches = highestLevelStack.match(lineRegExp);
         let match = matches ? matches[0] : null;
         if (match) {
             match = match.substring(1); // exclude semicolon
             const [line] = match.split(":");
+            console.log('\x1b[33m%s\x1b[0m', `Logger at line: ${line}. Jump: ${stackPath}\n`, ...args);
         }
-        console.log('\x1b[33m%s\x1b[0m', `Logger at line: ${match}.\n`, ...args);
     }
 }
 exports.default = Bootstrap;
