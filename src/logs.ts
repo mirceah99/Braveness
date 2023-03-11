359ff24da8dacb002173e53701c1d47b3bcbdbee
gabbster94 <b194gab@gmail.com>
2023-03-11 21:10:21 +0200

    Fixed line number; added path logic

--- src/logs.ts
+++ src/logs.ts
@@ -1,30 +1,35 @@
 export default class Bootstrap {
 
   private static generateError(): any {
-      try {
-          throw Error("");
-      } catch (err) {
-          return err;
-      }
+    try {
+      throw Error("");
+    } catch (err) {
+      return err;
+    }
   }
 
   static myLog(...args: any[]) {
-      const error = this.generateError();
+    const error = this.generateError();
+    const lineRegExp = /:[0-9]{1,9}:[0-9]{1,9}(\)?)/gm;
+    const pathRegExp = /(?<=\().*(:)/gm;
 
-      const lineRegExp = /:[0-9]{1,9}:[0-9]{1,9}(\)?)/gm;
-      const stackAsArray: string[] = error.stack.split("\n");
+    const stackAsArray: string[] = error.stack.split("\n");
+    const moduleStackIndex = stackAsArray.findIndex(stack => stack.includes('Module._compile'));
+    const highestLevelStack = stackAsArray[moduleStackIndex - 1];
+    let stackPath = highestLevelStack.match(pathRegExp)![0];
+    stackPath = stackPath.substring(0, stackPath.length - 1);
 
-      const errorStackLength: number = stackAsArray.length;
-      const highestLevelStack: string = stackAsArray[errorStackLength - 7];
+    const matches = highestLevelStack.match(lineRegExp);
+    let match = matches ? matches[0] : null;
 
-      const matches = highestLevelStack.match(lineRegExp);
-      let match = matches ? matches[0] : null;
+    if (match) {
+      match = match.substring(1); // exclude semicolon
+      const [line] = match.split(":");
 
-      if (match) {
-          match = match.substring(1); // exclude semicolon
-          const [line] = match.split(":");
-      }
-
-      console.log('\x1b[33m%s\x1b[0m', `Logger at line: ${match}.\n`, ...args);
+      console.log(
+        '\x1b[33m%s\x1b[0m',
+        `Logger at line: ${line}. Jump: ${stackPath}\n`,
+        ...args);
+    }
   }
 }
